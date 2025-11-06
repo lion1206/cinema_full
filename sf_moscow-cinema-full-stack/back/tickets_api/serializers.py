747c91246fcd79d4d2832652035e501bf7621c05
lion1206 <81245419+lion1206@users.noreply.github.com>
2025-11-06 14:20:15 +0300

    Delete sf_moscow-cinema-full-stack directory

--- sf_moscow-cinema-full-stack/back/tickets_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Ticket
-
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = ['id','ticket_number','movie_title','start_time','seat_number']
